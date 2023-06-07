7f89056802209f2ce83c24b5e41aa34efd3ade14
ath08 <ath0807@gmail.com>
2023-06-08 01:06:27 +0400

    type byte

---
+++ PrimitiveTypes.java
@@ -0,0 +1,21 @@
+class PrimitiveTypes {
+    public static void main(String[] args){
+        // byte data type is a 8-bit signed integer
+        // byte default value is 0
+        // min value is -128(-2^7)
+        // max value is 127 (2^7-1)
+
+        byte max = 127; 
+        byte min = -128;
+        //byte c = min + max; 
+        
+        System.out.println(min);
+        System.out.println(max);
+        //System.out.println(c);
+
+
+
+
+    }    
+
+}
