a2e4cebd4fd465b98d57baf4d2d749361f1fac07
ath08 <ath0807@gmail.com>
2023-06-07 23:49:17 +0400

    commiting Hello.java and Hello.class fails

---
+++ Hello.java
@@ -0,0 +1,30 @@
+class Hello {
+public static void main(String[] args){
+    System.out.println("Hello Anahit");
+
+    String firstName = "Tatev";
+    String lastName = "Aslikyan";
+
+    System.out.println(firstName + " " + lastName); 
+
+
+    int age = 32; 
+
+    System.out.println("Tatev's age  " + " " + age);
+
+    int num1 = 15;
+    float num2 = 5.5f;
+
+    System.out.println(num1 + num2);
+
+    System.out.println(num1 == num2);
+
+    char z = 106+80;
+
+    System.out.println(z);
+
+    System.out.println(num1 != num2);
+
+}    
+
+}
